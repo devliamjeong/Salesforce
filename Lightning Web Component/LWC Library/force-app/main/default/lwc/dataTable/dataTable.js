0cc38f3d925b000946ecba5c293a5d0658cab35d
Liam Jeong <dev.liam.jeong@gmail.com>
2021-03-02 20:33:27 -0600

    trailhead challenge completed

--- Lightning Web Component/LWC Library/force-app/main/default/lwc/dataTable/dataTable.js
+++ Lightning Web Component/LWC Library/force-app/main/default/lwc/dataTable/dataTable.js
@@ -1,14 +1,19 @@
 import { LightningElement } from 'lwc';
+import CONTACT_OBJECT from '@salesforce/schema/Contact';
+import NAME_FIELD from '@salesforce/schema/Contact.Name';
+import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+
 
 const columns=[
-    { label:'Name', fieldName:'name' },
-    { label:'Phone', fieldName:'phone'},
-    { label:'Email', fieldName:'email'}
+    { label:'Name', fieldName:'NAME_FIELD' },
+    { label:'Phone', fieldName:'PHONE_FIELD'},
+    { label:'Email', fieldName:'EMAIL_FIELD'}
 ];
 
 export default class DataTable extends LightningElement {
 
-    data=[];
+    data = CONTACT_OBJECT;
     columns = columns;
 
 }
